d172951c1f2839351c3442bbd0f2043116ba1fb0
YTeyfik <teyo2086@gmail.com>
2022-01-21 16:12:53 +0300

    Week 5 assigment 2 done

---
+++ Northwind.Entity/Dto/DtoRegister.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Entity.Dto
+{
+    public class DtoRegister
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string UserLastName { get; set; }
+        [Required]
+        public string UserCode { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Northwind.Entity/Models/UserRefreshToken.cs
@@ -0,0 +1,20 @@
+﻿using Northwind.Entity.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Entity.Models
+{
+    public class UserRefreshToken : EntityBase
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiration { get; set; }
+    }
+}
